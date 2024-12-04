ad7668d7abd6ec2356b07ecc6f77c9e15dcfaf48
nhoss6 <mohamed.djabi@outlook.fr>
2024-12-04 15:05:20 +0100

    fix

--- blog_project/blog/blog.py
+++ blog_project/blog/blog.py
@@ -1,29 +1,44 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Post, Category
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at')
+    list_display = ('name', 'slug', 'post_count')
     search_fields = ['name']
-    list_per_page = 20
     prepopulated_fields = {'slug': ('name',)}
+    ordering = ['name']
+    list_per_page = 20
+
+    def post_count(self, obj):
+        """Return the number of posts associated with a category."""
+        return obj.posts.count()
+    post_count.short_description = "Nombre d'articles"
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'category', 'created_at')
+    list_display = ('title', 'author', 'category', 'created_at', 'status_display')
     list_filter = ('category', 'created_at', 'author')
     search_fields = ('title', 'content', 'author__username')
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ('author',)
     date_hierarchy = 'created_at'
     list_per_page = 20
+    readonly_fields = ('created_at',)
 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "category":
-            kwargs["queryset"] = Category.objects.order_by('name')
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    def status_display(self, obj):
+        """Show a status indicator for published posts."""
+        return format_html(
+            '<span style="color: {}; font-weight: bold;">{}</span>',
+            '#28a745' if obj.pk else '#dc3545',
+            '✓ Publié' if obj.pk else '⨉ Non publié'
+        )
+    status_display.short_description = 'Statut'
 
     def save_model(self, request, obj, form, change):
-        if not obj.pk:  # If this is a new object
+        """Automatically assign the logged-in user as the author of a new post."""
+        if not obj.pk:  # If it's a new object
             obj.author = request.user
         super().save_model(request, obj, form, change)
--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from django.urls import reverse
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Nom")
     slug = models.SlugField(unique=True, blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = "Categories"
+        verbose_name = "Catégorie"
+        verbose_name_plural = "Catégories"
         ordering = ['name']
 
     def save(self, *args, **kwargs):
+        """Automatically generate slug if not provided."""
         if not self.slug:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
@@ -18,12 +22,27 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        """Return the URL of a category's detail page."""
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
+
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, verbose_name="Titre")
+    content = models.TextField(verbose_name="Contenu")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Date de création")
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name="Auteur"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name="Catégorie"
+    )
     slug = models.SlugField(unique=True, blank=True, null=True)
 
     class Meta:
@@ -32,9 +51,18 @@ class Post(models.Model):
         verbose_name_plural = "Articles"
 
     def save(self, *args, **kwargs):
+        """Automatically generate slug if not provided."""
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        """Return the URL of a post's detail page."""
+        return reverse('post_detail', kwargs={'slug': self.slug})
+
+    def get_excerpt(self, chars=200):
+        """Return an excerpt of the content with a default length of 200 characters."""
+        return self.content[:chars] + '...' if len(self.content) > chars else self.content
